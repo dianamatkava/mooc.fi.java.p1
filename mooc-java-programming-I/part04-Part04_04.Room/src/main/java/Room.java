d69761d6655409b5abe8206d6deed4fe1e335d67
dianamatkava <dianamatkava@gmail.com>
2024-11-19 09:00:50 +0100

    work on part 4 OOP

---
+++ mooc-java-programming-I/part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,9 @@
+public class Room {
+    String code;
+    int seats;
+    
+    public Room (String classCode, int numberOfSeats) {
+        this.code = classCode;
+        this.seats = numberOfSeats;
+    }
+}
